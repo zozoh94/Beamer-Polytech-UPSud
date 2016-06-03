81258c6604c43a5d3bbff24a0a17ffaf47b053e9
Thom Castermans <thomcastermans@users.sourceforge.net>
2016-06-03 15:25:01 +0200

    Update list of contributors
    Also automate this update step with a little Ruby script.

---
+++ _scripts/update-readme.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'open-uri'
+
+#
+# Given an array of strings, return a human-readable concatenation of that list.
+# For example, ['foo', 'bar', 'baz'] will become 'foo, bar and baz'.
+# In fact, this function assumes that items are hashes with a :name and :url and
+# will generate links in the returned result.
+#
+def csl(arr)
+  return '' if arr.length === 0
+  return lnk(arr[0]) if arr.length === 1
+
+  arr.map!{ |c| '<a href="%{url}">%{name}</a>' % c }
+  last = arr.pop
+  ret = arr.join(', ')
+  ret += ' and ' + last
+end
+
+repo_contributors = JSON.parse(open('https://api.github.com/repos/Caster/Beamer-TUe/contributors').read)
+contributors = []
+out = 'With contributions by '
+
+repo_contributors.each{ |c| contributors.push({name: c['login'], url: c['html_url']}) unless c['login'] === 'Caster' }
+out += csl(contributors)
+
+write = File.join(File.dirname(__FILE__), '..', '_includes', 'contributors.html')
+if File.exists?(write) and File.writable?(write)
+  File.open(write, 'w') { |f| f.puts out }
+else
+  puts out
+end
